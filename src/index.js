4b51f1f284235e61b9dcb4577e6efb015159df98
praphulksingh <140788294+praphulksingh@users.noreply.github.com>
2023-10-30 11:40:27 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <div>
+    <h1>My fav food</h1>
+    <ul>
+      <li>mango</li>
+      <li>orange</li>
+      <li>grapes</li>
+    </ul>
+  </div>,
+  document.getElementById("root")
+);
